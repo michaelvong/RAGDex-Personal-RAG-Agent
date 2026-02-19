56115373c2f9a6f90bb968f6d0a1bbd12dc59572
michaelvong <mvong006@ucr.edu>
2026-02-18 22:56:29 -0800

    cleaned code

--- rag_agent/scripts/ingest_docs.py
+++ rag_agent/scripts/ingest_docs.py
@@ -1,11 +1,13 @@
 from rag_agent.app.ingestion.document_loader import DocumentLoader
 
+BASE_DIR = "C:\\Users\\Michael\\PycharmProjects\\PersonalRAG\\rag_agent\\"
+
 # Initialize loader
 loader = DocumentLoader(
-    raw_dir="C:\\Users\\Michael\\PycharmProjects\\PersonalRAG\\rag_agent\data\\raw",
-    processed_dir="C:\\Users\\Michael\\PycharmProjects\\PersonalRAG\\rag_agent\data\\processed",
+    raw_dir= BASE_DIR + "data\\raw",
+    processed_dir= BASE_DIR + "data\\processed\\chunks",
     chunk_size=5,
-    log_file="data/processed/processed_files.json",
+    log_file= BASE_DIR + "data\\processed\\logs\\processed_files.json",
     use_hashing=False
 )
 
