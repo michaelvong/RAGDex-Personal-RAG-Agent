40975a35a42294703ee05bdeb35d0906b8183c7a
michaelvong <mvong006@ucr.edu>
2026-02-23 13:36:54 -0800

    script to run create embeddings

--- rag_agent/scripts/get_embeddings.py
+++ rag_agent/scripts/get_embeddings.py
@@ -3,10 +3,8 @@ from rag_agent.app.ingestion.create_embeddings import ChunkVectorizer
 vectorizer = ChunkVectorizer(
     model_name="sentence-transformers/all-MiniLM-L6-v2",
     chunk_dir="C:\\Users\\Michael\\PycharmProjects\\PersonalRAG\\rag_agent\\data\\processed\\chunks",
+    output_dir="C:\\Users\\Michael\\PycharmProjects\\PersonalRAG\\rag_agent\\data\\processed\\embeddings",
     batch_size=64
 )
 
-vectorizer.run_pipeline(output_file="C:\\Users\\Michael\\PycharmProjects\\PersonalRAG\\rag_agent\\data\\processed\\embeddings\\embeddings.json")
-
-print(vectorizer.embeddings[0].shape)
-print(vectorizer.metadata[0])
+vectorizer.run_pipeline()
